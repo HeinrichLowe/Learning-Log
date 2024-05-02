6bcee5502cf4e999c077c1da4ddaf8935e806ce7
HeinrichLowe <haimirichlowe@hotmail.com>
2024-05-02 14:45:07 -0300

    adding the 'new_topic' and 'new_entry' page and logic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
+from django.urls import reverse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -18,3 +21,35 @@ def topic(request, topic_id):
     context = {'topic': topic, 'entries': entries}
 
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('topics'))
+    
+    context = {'form': form}
+
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id = topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data = request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('topic', args=[topic_id]))
+        
+    context = {'topic': topic, 'form': form}
+
+    return render(request, 'learning_logs/new_entry.html', context)
