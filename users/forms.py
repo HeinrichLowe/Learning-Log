4120eb1df4adef3dd9cf79c5d289504c007b9855
HeinrichLowe <HaimirichLowe@hotmail.com>
2024-08-02 16:49:36 -0300

    Updated dependencies and made some code changes.

--- users/forms.py
+++ users/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Remover as dicas do Username
+        # Remove username tips.
         self.fields['username'].help_text = None
 
-        # Remova as dicas de senha
+        # Remove password tips.
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
